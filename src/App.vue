fd4089fa5ea65d4d94e97d5ba6cf74fbc1233f87
Nathanael Lafeuille <akai.chesha@gmail.com>
2021-03-29 11:23:44 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    {{ msg }}
+    {{ splittedMsg() }}
+    {{ getNumberInMsg() }}
+</template>
+
+<script lang="ts"></script>
+<script lang="ts" setup>
+ref: msg = "Hello World 42 !";
+
+// This works fine
+const splittedMsg = () => msg.split(" ");
+
+// This does not
+const getNumberInMsg = () => {
+    if (!msg) return null;
+    const [currMatch] = msg.match(/\d+/) ?? [];
+    if (!currMatch) return null;
+    return Number(currMatch);
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
